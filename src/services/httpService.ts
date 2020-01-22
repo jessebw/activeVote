1ce030901d4a1e975ae570b56e8dbb7552e4ce04
jesse walsh <jessewalsh@jesse-3.local>
2020-01-22 20:18:19 +1300

    feature:25

--- src/services/httpService.ts
+++ src/services/httpService.ts
@@ -1,152 +1,149 @@
-import fetchIntercept from "fetch-intercept";
-import userService from "./userService";
-import jwtDecode from "jwt-decode";
+import fetchIntercept from 'fetch-intercept'
+import userService from './userService'
+import jwtDecode from 'jwt-decode'
 
 fetchIntercept.register({
-  request: function(url, config = {}) {
-    // Modify the url or config here
-    // const authorization = config.headers.{authorization}
-    const auth = userService.auth;
-    console.log("auth", auth);
-    // config = config || {};
-    console.log("authConfig:", config);
+  request: function (url, config = {}) {
+    const auth = userService.auth
+    // console.log("auth", auth);
+    // console.log("authConfig:", config);
     if (auth) {
       config.headers = {
         ...config.headers,
-        Authorization: "Bearer " + auth.token
-      };
-      console.log("token", auth.token);
+        Authorization: 'Bearer ' + auth.token
+      }
+      // console.log('token', auth.token)
     }
-    return [url, config];
+    return [url, config]
   },
 
-  requestError: function(error) {
+  requestError: function (error) {
     // Called when an error occured during another 'request' interceptor call
-    return Promise.reject(error);
+    return Promise.reject(error)
   },
 
-  response: function(response) {
+  response: function (response) {
     // Modify the reponse object
-    console.log("response", response);
-    return response;
+    // console.log("response", response);
+    return response
   },
-  responseError: function(error) {
+  responseError: function (error) {
     // Handle an fetch error
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-});
+})
 
 class HttpService {
-  static getInstance() {
+  static getInstance () {
     if (!HttpService.instance) {
-      HttpService.instance = new HttpService();
+      HttpService.instance = new HttpService()
     }
-    console.log("httpService Instance Given");
-    return HttpService.instance;
+    // console.log('httpService Instance Given')
+    return HttpService.instance
   }
 
-  private static instance: HttpService;
+  private static instance: HttpService
 
-  private constructor() {
+  private constructor () {
     // default for all requests
   }
 
-  get(url: string) {
-    return fetch(url).then(function(response) {
+  get (url: string) {
+    return fetch(url).then(function (response) {
       if (response.status !== 200) {
         console.log(
-          "Looks like there was a problem. Status Code: " + response.status
-        );
+          'Looks like there was a problem. Status Code: ' + response.status
+        )
 
         if (response.status === 401) {
-          const auth = JSON.parse(sessionStorage.getItem("auth") as string);
-          const decoded: any = jwtDecode(auth.token);
-          console.log("decoded token", decoded);
+          const auth = JSON.parse(sessionStorage.getItem('auth') as string)
+          const decoded: any = jwtDecode(auth.token)
+          console.log('decoded token', decoded)
 
           return HttpService.getInstance()
-            .post("http://activevoteserver.deverall.co.nz/refreshtoken", {
+            .post('http://activevoteserver.deverall.co.nz/refreshtoken', {
               email: decoded.email,
               refreshToken: auth.refreshToken
             })
             .then(data => {
-              sessionStorage.setItem("auth", JSON.stringify(data));
+              sessionStorage.setItem('auth', JSON.stringify(data))
               // console.log("token", this.auth && this.auth.token);
-              userService.readSessionState();
+              userService.readSessionState()
               return fetch(url).then(successfullResponse => {
                 return successfullResponse.json().then(data => {
-                  console.log("refreshed Token", data);
-                  return data;
-                });
-              });
-            });
+                  console.log('refreshed Token', data)
+                  return data
+                })
+              })
+            })
         }
 
-        return Promise.reject();
+        return Promise.reject()
       }
 
       return response.json().then(data => {
-        console.log(data);
-        return data;
-      });
-    });
+        // console.log(data)
+        return data
+      })
+    })
   }
 
-  post(url: string, bodyData: any) {
+  post (url: string, bodyData: any) {
     return fetch(url, {
       body: JSON.stringify(bodyData),
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json'
       }
-    }).then(function(response) {
+    }).then(function (response) {
       if (response.status !== 200) {
         throw new Error(
-          "Looks like there was a problem. Status Code: " + response.status
-        );
+          'Looks like there was a problem. Status Code: ' + response.status
+        )
       }
       return response.json().then(data => {
-        console.log(data);
-        return data;
-      });
-    });
+        // console.log(data)
+        return data
+      })
+    })
   }
 
-  put(url: string, bodyData: any) {
+  put (url: string, bodyData: any) {
     return fetch(url, {
       body: JSON.stringify(bodyData),
-      method: "PUT",
+      method: 'PUT',
       headers: {
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json'
       }
-    }).then(function(response) {
+    }).then(function (response) {
       if (response.status !== 200) {
         console.log(
-          "Looks like there was a problem. Status Code: " + response.status
-        );
-        return;
+          'Looks like there was a problem. Status Code: ' + response.status
+        )
+        return
       }
       return response.json().then(data => {
-        console.log(data);
-        return data;
-      });
-    });
+        // console.log(data)
+        return data
+      })
+    })
   }
 
-  delete(url: string) {
+  delete (url: string) {
     return fetch(url, {
-      method: "DELETE",
+      method: 'DELETE',
       headers: {
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json'
       }
-    }).then(function(response) {
+    }).then(function (response) {
       if (response.status !== 200) {
         throw new Error(
-          "Looks like there was a problem. Status Code: " + response.status
-        );
+          'Looks like there was a problem. Status Code: ' + response.status
+        )
       }
-      return "Successfully Deleted all of your data";
-    });
+      return 'Successfully Deleted all of your data'
+    })
   }
 }
 
-export default HttpService.getInstance();
+export default HttpService.getInstance()
